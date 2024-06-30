62da9f94e3a99ebfe3da21d8fb1a2ab61b159fbb
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-06-30 15:44:41 +0200

    Initialized typescript Hardhat project

---
+++ hardhat.config.ts
@@ -0,0 +1,30 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import { vars } from "hardhat/config";
+
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      chainId: 11155111,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true
+  },
+};
+
+export default config;
