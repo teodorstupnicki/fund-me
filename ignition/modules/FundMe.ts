1a3858bdc5818a6f79bcd1d212f106c757db1f1d
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-07-01 16:15:16 +0200

    Updated ignition modules

--- ignition/modules/FundMe.ts
+++ ignition/modules/FundMe.ts
@@ -1,9 +1,24 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import { network } from "hardhat";
+import verify from "../../utils/verify";
+import { networkConfig, developmentChains } from "../../helper-hardhat-config";
 
 export default buildModule("FundMeModule", (m) => {
-  const apollo = m.contract("FundMe", ["Saturn V"]);
+  const chainId = network.config.chainId; 
+  const deployer = m.getAccount(0);
+  const tokenOwner = m.getAccount(1);
 
-  m.call(apollo, "launch", []);
-
-  return { apollo };
+  let ethUsdPriceFeedAddress: string
+  if (chainId == 31337) {
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+    ethUsdPriceFeedAddress = ethUsdAggregator.address
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[network.name].ethUsdPriceFeed!
+  }
+  
+  const fundMe = m.contract("FundMe", [ethUsdPriceFeedAddress], {
+    from: deployer,
+  });
+  
+  return { fundMe };
 });
